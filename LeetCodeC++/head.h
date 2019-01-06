d4d091ed774cdc9f3ac1805403d7d9635c74ed48
liangjiakun <1023587725@qq.com>
2019-01-06 10:06:06 +0800

    adjust directory

---
+++ LeetCodeC++/head.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <cstdio>
+#include <math.h>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <algorithm>
+#include <tchar.h>
+#include <stack>
+#include <numeric>
+using namespace std;
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # LeetCode
-Record my daily practice in LeetCode.
+记录了LeetCode上每道题的不同方法，以及解题思路
+
+## LeetCodeC++
+环境：VS 2017
+
+语言：C++
+
+## LeetCodeJava
+环境：IDEA 2018
+
+语言：Java
+
+## LeetCodePython
+环境：Pycharm 2018
+
+语言：Python
